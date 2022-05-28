35114ad76d888e01f96b5e43bcc0b23854b161c1
bisrat-walle <bisratwalle3@gmail.com>
2022-05-28 09:49:33 +0300

    modifying dfs implementation

--- graphsearch/dfs.py
+++ graphsearch/dfs.py
@@ -1,15 +1,15 @@
+from collections import deque
+
+
 class DFS:
-    def __init__(self, graph, to_be_searched):
+    def __init__(self, graph):
         self.graph = graph
-        self.to_be_searched = to_be_searched
     
-    def bfs(graph, to_be_searched = -1):
-        print("Entered")
-        stack = deque([graph])
+    def search(self, to_be_searched = -1):
+        stack = deque([self.graph])
         while stack:
             current = stack.pop()
             current.visited = True
-            print(current.value, "marked True")
             if current.value == to_be_searched:
                 return True
             if current.left:
