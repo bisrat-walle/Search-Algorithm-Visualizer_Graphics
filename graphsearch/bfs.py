46ef277434bfd878f631dcb68adbbc904881cfcc
bisrat-walle <bisratwalle3@gmail.com>
2022-05-28 09:49:19 +0300

    modifying bfs implementation

--- graphsearch/bfs.py
+++ graphsearch/bfs.py
@@ -1,15 +1,14 @@
+from collections import deque
+
 class BFS:
-    def __init__(self, graph, to_be_searched):
+    def __init__(self, graph):
         self.graph = graph
-        self.to_be_searched = to_be_searched
     
-    def bfs(graph, to_be_searched = -1):
-        print("Entered")
+    def search(self, to_be_searched = -1):
         queue = deque([self.graph])
         while queue:
             current = queue.popleft()
             current.visited = True
-            print(current.value, "marked True")
             if current.value == to_be_searched:
                 return True
             if current.left:
