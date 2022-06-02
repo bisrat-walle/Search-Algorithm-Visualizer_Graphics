e80bb5389f015abf5dca389aaa6a822d287669a2
Yeabsira <dryeab@gmail.com>
2022-06-02 10:35:19 +0300

    minor bug fixes

--- array_search/drawer.py
+++ array_search/drawer.py
@@ -7,22 +7,10 @@ from pygame.locals import *
 import numpy as np
 
 
-def init():
-    pygame.init()
-    display = (500, 500)
-    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
-    glClearColor(0.0, 0.0, 0.0, 1.0)
-    gluOrtho2D(-10.0, 10.0, -10.0, 10.0)
+def drawText(x: int, y: int, text: int or str, color: tuple, fontSize: int = 25):
 
-
-def drawText(x, y, text, fontSize=25):
-
-    glClear(GL_COLOR_BUFFER_BIT)
-
-    font = pygame.font.SysFont('arial', fontSize)
-
-    textSurface = font.render(
-        str(text), True, (0, 255, 0, 255), (255, 255, 255, 255))
+    textSurface = pygame.font.SysFont('arial', fontSize).render(
+        str(text), True, (0, 255, 0, 255), (1, 1, 1, 255))
     textData = pygame.image.tostring(textSurface, "RGBA", True)
 
     glWindowPos2d(x, y)
@@ -30,37 +18,42 @@ def drawText(x, y, text, fontSize=25):
                  GL_RGBA, GL_UNSIGNED_BYTE, textData)
 
 
-def drawCircle(ox: int, oy: int, r: int, segment: int, num: int, color: tuple = (240, 1, 1)):
+def drawCircle(ox: int, oy: int, r: int, num: int, color: tuple = (1, 0, 0)):
     """ Draws a circle with radius <r> from origin <ox, oy> """
 
-    drawText(ox, oy, "1008")
+    drawText(3, 0, 1000, (10, 0, 10))
 
     glPolygonMode(GL_BACK, GL_FILL)
     glColor3f(*color)
 
     glBegin(GL_POLYGON)
 
-    for i in range(segment):
-        angle = 2 * pi * i / segment
+    for i in range(1000):
+        angle = 2 * pi * i / 1000
 
         x = r * cos(angle) + ox
         y = r * sin(angle) + oy
 
         glVertex2f(x, y)
 
-    glEnd()
-    glFlush()
+    print('x is ', x, 'y is ', y)
 
 
 def main():
-    init()
+
+    pygame.init()
+    display = (500, 500)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    gluOrtho2D(-10.0, 10.0, -10.0, 10.0)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        drawCircle(0, 0, 3, 10**3, 10)
+        drawCircle(0, 0, 3, 10)
 
         pygame.display.flip()
         pygame.time.wait(10)
