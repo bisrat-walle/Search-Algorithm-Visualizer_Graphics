bbb1754ede01b8a7686d0cc2ec4a7d909fab6056
Yeabsira <dryeab@gmail.com>
2022-06-02 08:34:48 +0300

    added the random list generator file and setup the linear search

--- arraysearch/binary_search.py
+++ arraysearch/binary_search.py
@@ -4,6 +4,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from pygame.locals import *
 import numpy as np
+import time
 
 root = Tk()
 root.title('Binary Search Simulation')
@@ -17,3 +18,19 @@ def init(self):
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
     glClearColor(0.0, 0.0, 0.0, 1.0)
     gluOrtho2D(-10.0, 10.0, -4.0, 4.0)
+
+
+def binary_search(nums, target, time):
+
+    left, right = 0, len(nums) - 1
+
+    while left < right:
+        mid = (left + right)//2
+        if nums[mid] == target:
+            return mid, True
+        if nums[mid] < target:
+            left = mid + 1
+        else:
+            left = mid - 1
+
+    return mid
---
+++ arraysearch/random_list_generator.py
@@ -0,0 +1,9 @@
+import random
+
+
+def random_list_generator(sort: bool = False, n: int = 10):
+    """ Generates random list of length <n> """
+
+    lst = [int(random.random() * 20) for _ in range(n)]
+
+    return sorted(lst) if sort else lst
