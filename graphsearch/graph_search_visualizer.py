3a88cc3bf33944a34114289890517b9fccd9f854
bisrat-walle <bisratwalle3@gmail.com>
2022-06-07 22:19:11 +0300

    removing unnecessary edge

--- graphsearch/graph_search_visualizer.py
+++ graphsearch/graph_search_visualizer.py
@@ -10,7 +10,52 @@ from graphsearch.random_graph_generator import *
 
 coordinateSize = (10, 6)
 window_size = (1000, 600)
-    
+pygame.init()
+COLOR_INACTIVE = pygame.Color('lightskyblue3')
+COLOR_ACTIVE = pygame.Color('dodgerblue2')
+FONT = pygame.font.Font(None, 32)
+
+class InputBox:
+
+    def __init__(self, x, y, w, h, text=''):
+        self.rect = pygame.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
+
+    def update(self):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width()+10)
+        self.rect.w = width
+
+    def draw(self, screen):
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
+        # Blit the rect.
+        pygame.draw.rect(screen, self.color, self.rect, 2)
 
 class GraphAlgorithmVisualizer:
     """
@@ -29,7 +74,6 @@ class GraphAlgorithmVisualizer:
             
         
         """
-        pygame.init()
         self.window = pygame.display.set_mode(window_size, DOUBLEBUF|OPENGL)
         gluOrtho2D(-1*coordinateSize[0], coordinateSize[0], -1*coordinateSize[1], coordinateSize[1])
         pygame.display.set_caption("Search Algorithm Visualizer")
@@ -122,8 +166,13 @@ class GraphAlgorithmVisualizer:
         search_generator = None
         iterations = 1
         finished = False
+        input_box1 = InputBox(100, 100, 140, 32)
+        input_boxes = [input_box1]
         while True:
+            
             for event in pygame.event.get():
+                for box in input_boxes:
+                    box.handle_event(event)
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     quit()
@@ -188,6 +237,8 @@ class GraphAlgorithmVisualizer:
                     
                 startY -= 1.5
                 i += 1
+            for box in input_boxes:
+                pygame.Surface.blit(box, self.window, (0, 0))
             pygame.display.flip()
             pygame.time.wait(10)
             
