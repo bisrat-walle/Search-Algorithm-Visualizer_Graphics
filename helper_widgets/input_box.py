687116a8f02771826955cad2da3b4afd35a7753c
bisrat-walle <bisratwalle3@gmail.com>
2022-06-10 23:58:10 +0300

    finishing input reciver widget

--- helper_widgets/input_box.py
+++ helper_widgets/input_box.py
@@ -1,11 +1,18 @@
 import pygame as pg
+from pygame.locals import *
 
+# NOFRAME 
 
 pg.init()
-screen = pg.display.set_mode((640, 480))
+screen = pg.display.set_mode((400, 170), NOFRAME)
 COLOR_INACTIVE = pg.Color('lightskyblue3')
 COLOR_ACTIVE = pg.Color('dodgerblue2')
+GRAY = (30, 30, 30)
 FONT = pg.font.Font(None, 32)
+
+green = (0, 255, 0)
+red = (255, 0, 0)
+blue = (0, 0, 128)
 
 
 class InputBox:
@@ -19,13 +26,10 @@ class InputBox:
 
     def handle_event(self, event):
         if event.type == pg.MOUSEBUTTONDOWN:
-            # If the user clicked on the input_box rect.
             if self.rect.collidepoint(event.pos):
-                # Toggle the active variable.
                 self.active = not self.active
             else:
                 self.active = False
-            # Change the current color of the input box.
             self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
         if event.type == pg.KEYDOWN:
             if self.active:
@@ -36,33 +40,44 @@ class InputBox:
                     self.text = self.text[:-1]
                 else:
                     self.text += event.unicode
-                # Re-render the text.
                 self.txt_surface = FONT.render(self.text, True, self.color)
 
     def update(self):
-        # Resize the box if the text is too long.
-        width = max(200, self.txt_surface.get_width()+10)
+        width = max(50, self.txt_surface.get_width()+10)
         self.rect.w = width
 
     def draw(self, screen):
-        # Blit the text.
         screen.blit(self.txt_surface, (self.rect.x+5, self.rect.y+5))
-        # Blit the rect.
         pg.draw.rect(screen, self.color, self.rect, 2)
 
+def drawText(text, x, y, color, fontSize=20):
+    font = pg.font.Font('freesansbold.ttf', fontSize)
+    text = font.render(text, True, color)
+    textRect = text.get_rect()
+    textRect.center = (x, y)
+    screen.blit(text, textRect)
 
 
-def main():
+def main(alg="BFS"):
     clock = pg.time.Clock()
-    input_box1 = InputBox(100, 100, 140, 32)
-    input_box2 = InputBox(100, 300, 140, 32)
-    input_boxes = [input_box1, input_box2]
+    input_box1 = InputBox(340, 50, 140, 30)
+    input_boxes = [input_box1]
+    button1 = pg.Rect(210, 100, 150, 40)
+    button2 = pg.Rect(40, 100, 150, 40)
     done = False
 
     while not done:
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 done = True
+            elif event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if button1.collidepoint(event.pos):
+                        done=True
+                        print("Clicked 1")
+                    if button2.collidepoint(event.pos):
+                        done=True
+                        print("Clicked 2")
             for box in input_boxes:
                 box.handle_event(event)
 
@@ -72,11 +87,17 @@ def main():
         screen.fill((30, 30, 30))
         for box in input_boxes:
             box.draw(screen)
+        pg.draw.rect(screen, green, button1)
+        pg.draw.rect(screen, red, button2)
+        drawText("CANCEL", 116, 120, blue)
+        drawText("SEARCH", 280, 120, blue)
+        drawText("Enter the number to be searched: ", 170, 65, green)
+        drawText(f"Algorithm: {alg}", 170, 30, green, 22)
 
         pg.display.flip()
         clock.tick(30)
-
-
-if __name__ == '__main__':
-    main()
+    
     pg.quit()
+
+
+main()
