40394e159d8b169cea0f3654ed7eff5e358e0b14
bisrat-walle <bisratwalle3@gmail.com>
2022-06-16 10:46:31 +0300

    adding splash screen which also recieves algorithm type from the user

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from common.splash_screen import *
 from graphsearch.graph_search_visualizer import *
             
 if __name__ == "__main__":
-    GraphAlgorithmVisualizer()
+    choosen = SplashUserInput().getInput()
+    print(f"Choosen Alg Type: {choosen}")
+    if choosen == "Graph Search":
+        GraphAlgorithmVisualizer()
+    else:
+        print("We are currently developing it (stay tuned)")
+        
